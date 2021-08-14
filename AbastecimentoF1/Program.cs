aa237b78f77fba8cf8cc91cc9dda6fe9853d9a27
Jeferson Melo <jefsdemelo@gmail.com>
2021-08-14 13:18:22 -0300

    Solução Final

--- AbastecimentoF1/Program.cs
+++ AbastecimentoF1/Program.cs
@@ -6,7 +6,24 @@ namespace AbastecimentoF1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+
+			Console.WriteLine("Entre com a extensão da pista");
+			double tPista = double.Parse(Console.ReadLine());  //3,5
+
+			Console.WriteLine("Entre com o número de voltas do GP");
+			int nVoltas = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Entre com número de reabastecimentos desejados");
+			int nAbastecimentos = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Entre com o consumo de combustível do carro (em Km/L)");
+			double consumo = double.Parse(Console.ReadLine());//2
+
+			double pitStop = (nVoltas / nAbastecimentos) / consumo;
+			double resposta = pitStop * tPista;
+
+
+			Console.WriteLine($"litros necessários para percorrer até o primeiro reabastecimento: {resposta}");
 		}
 	}
 }
